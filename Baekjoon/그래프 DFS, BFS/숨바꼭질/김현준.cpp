f14a351664fa122c9c63a11b45579eb708ce525b
june <guswns694@gmail.com>
2025-05-07 09:07:20 +0900

    solved DFS와BFS(1260) 김현준.cpp

--- Baekjoon/그래프 DFS, BFS/숨바꼭질/김현준.cpp
+++ Baekjoon/그래프 DFS, BFS/숨바꼭질/김현준.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
-const int maxpoint =100001;
-int visited[maxpoint];
-void bfs(int s, int m){
+const int maxpoint =1001;
+vector<int> pv[maxpoint];
+bool visited[maxpoint];
+void dfs(int s){
+    if(visited[s])
+        return;
+    visited[s]=1;
+    cout<<s<<' ';
+    for(int i=0;i<pv[s].size();i++)
+    {
+        dfs(pv[s][i]);    
+    }
+}
+void bfs(int s){
+    fill(visited,visited+maxpoint,0);
     queue<int> q;
     q.push(s);
+    visited[s]=1; 
     while(!q.empty()){
         int c=q.front();
-        if(c==m)
-        {
-            cout<<visited[c];
-            return ;
-        }
         q.pop();
-        int next[3]={c+1,c-1,c*2};
-        for(int i=0;i<3;i++)
+        cout<<c<<' ';
+        for(int i=0;i<pv[c].size();i++)
         {
-            if(next[i]>100001)
-                continue;
-            if(visited[next[i]]==0)
+            int next =pv[c][i];
+            if(!visited[next])
             {
-                q.push(next[i]);
-                visited[next[i]]=visited[c]+1;
+                q.push(next);
+                visited[next]=true;
+                
             }
         }
-        
     }
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n,m;
-    cin>>n>>m;
-    bfs(n,m);
+    int n,m,v,t,t2;
+    cin>>n>>m>>v;
+    for(int i=0;i<m;i++)
+    {
+       cin>>t>>t2;
+       pv[t].push_back(t2);
+       pv[t2].push_back(t);
+    }
+    for (int i = 1; i <= n; i++) {
+        sort(pv[i].begin(), pv[i].end());  // 번호 작은 순서로 정렬
+    }
+    dfs(v);
+    cout<<"\n";
+    bfs(v);
     return 0;
 }
